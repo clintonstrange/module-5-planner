cf23dd35500026e69d40cb701d3ca534941d5be3
clintonstrange <71712425+clintonstrange@users.noreply.github.com>
2020-10-28 19:40:05 -0500

    updated icons

--- Develop/script.js
+++ Develop/script.js
@@ -1,29 +1,72 @@
 var eventEl = $(".future");
 var hourEl = $(".hour");
+var events = {};
 
-var loadDate = function () {
+var loadPlanner = function () {
   var todaysDate = moment().format("dddd, MMMM Do YYYY");
   $("#currentDay").append(todaysDate);
   auditTime(hourEl);
+  events = JSON.parse(localStorage.getItem("events"));
+  if (!events) {
+    events = {
+      text: $(".event").text().trim(),
+    };
+  }
 };
+
+var saveEvents = function () {
+  localStorage.setItem("events", JSON.stringify(events));
+};
+
+$(".time-block").on("click", ".event", function () {
+  var text = $(this).text().trim();
+  var textInput = $("<textarea>").addClass("event col-10").val(text);
+  $(this).replaceWith(textInput);
+  textInput.trigger("focus");
+});
+
+$(".time-block").on("blur", "textarea", function () {
+  var text = $(this).val().trim();
+
+  events.text = events;
+  saveTasks();
+
+  var textP = $("<p>").addClass("event col-10").text(text);
+
+  $(this).replaceWith(textP);
+  auditTime(hourEl);
+});
+
+$("time-block").on("click", ".saveBtn", function () {
+  saveEvents();
+});
 
 var auditTime = function (timeEl) {
   for (var i = 0; i < timeEl.length; i++) {
-    var time = $(timeEl[i]).text().trim();
-    var convertedTime = moment(time, "H");
-    // console.log(convertedTime);
-    // console.log(moment(time));
-    // console.log(Math.abs(moment().diff(convertedTime, "hours")));
-    if (moment().isAfter(convertedTime)) {
-      $(eventEl[i]).removeClass("future");
-      $(eventEl[i]).addClass("past");
-    } else if (Math.abs(moment().diff(convertedTime, "hours")) < 1) {
+    var time = $(timeEl[i]).attr("id");
+    //console.log(time);
+    var currentTime = moment().format("HH");
+    //console.log(currentTime);
+
+    if (currentTime === time) {
+      //console.log("present");
       $(eventEl[i]).removeClass("future");
       $(eventEl[i]).addClass("present");
+    }
+    if (currentTime > time) {
+      //console.log("past");
+      $(eventEl[i]).removeClass("future");
+      $(eventEl[i]).addClass("past");
     }
   }
 };
 
-loadDate();
+setInterval(function () {
+  $(".container .time-block").each(function (index, el) {
+    auditTask(el);
+  });
+}, 1000 * 60 * 5);
+
+loadPlanner();
 
 //each hour will be col-1 and the event-info will take up col-10 with the save button being col-1
